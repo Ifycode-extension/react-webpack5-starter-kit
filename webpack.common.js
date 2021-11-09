e170bb30893993b0d48837569267786f284bb5a2
Ifycode <marychord@gmail.com>
2021-11-09 03:20:18 +0100

    Add .editorconfig (and impose rules on files that don't previously use it)

--- webpack.common.js
+++ webpack.common.js
@@ -6,8 +6,8 @@ const common = {
     index: join(__dirname, 'src', 'index.js')
   },
   plugins: [
-     //create HTML file that includes reference to bundled js
-     new HtmlWebpackPlugin({
+    //create HTML file that includes reference to bundled js
+    new HtmlWebpackPlugin({
       template: 'public/index.html'
     })
   ]
--- webpack.dev.js
+++ webpack.dev.js
@@ -9,51 +9,51 @@ if (platform === 'linux') chrome = 'google-chrome';
 if (platform === 'darwin') chrome = 'Google Chrome';
 
 const config = merge(common, {
-    mode: 'development',
-    devtool: 'inline-source-map',
-    output: {
-        filename: '[name].js',
-        path: resolve(__dirname, '/public/'),
-        publicPath: '/'
+  mode: 'development',
+  devtool: 'inline-source-map',
+  output: {
+    filename: '[name].js',
+    path: resolve(__dirname, '/public/'),
+    publicPath: '/'
+  },
+  devServer: {
+    static: {
+      directory: join(__dirname, 'public'),
     },
-    devServer: {
-        static: {
-          directory: join(__dirname, 'public'),
-        },
-        port: 5050,
-        open: {
-            app: {
-              name: chrome,
-              arguments: ['--new-window']
-            }
-        }
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/i,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-env', '@babel/preset-react']
-                    }
-                }
-            },
-            {
-                test: /\.(css|less)$/i,
-                use: [
-                'style-loader', 
-                'css-loader',
-                'less-loader'
-                ]
-            },
-            {
-                test: /\.svg$/,
-                use: ['@svgr/webpack']
-            }
-        ]
+    port: 5050,
+    open: {
+      app: {
+        name: chrome,
+        arguments: ['--new-window']
+      }
     }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/i,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      },
+      {
+        test: /\.(css|less)$/i,
+        use: [
+          'style-loader',
+          'css-loader',
+          'less-loader'
+        ]
+      },
+      {
+        test: /\.svg$/,
+        use: ['@svgr/webpack']
+      }
+    ]
+  }
 });
 
 export default config;
